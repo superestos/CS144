e361e394f9ccce61d5ba1cc1d315c7977a54eeac
superestos <superestos@gmail.com>
2021-01-15 00:24:58 +0800

    optimize reassembler to be disjoint

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -17,7 +17,7 @@ using namespace std;
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
     size_t begin = std::max(index, total_assembled);
-    size_t end = std::min(index + data.length(), _output.bytes_read() + _capacity);
+    size_t end = std::max(std::min(index + data.length(), _output.bytes_read() + _capacity), begin);
     if(ranges.count(begin) == 0 || ranges[begin] < end) {
         ranges[begin] = end;
     }
@@ -26,12 +26,26 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         buffer[(i % _capacity)] = data[i - index];
     }
 
-    if(begin == total_assembled) { // put contiguous string in stream
-        auto it = ranges.begin();
-        for(; it != ranges.end() && total_assembled >= it->first; ++it) {
-            total_assembled = std::max(total_assembled, it->second);         
+    auto it = ranges.find(begin);
+    if (it != ranges.begin()) {
+        it--;
+        if(it->second >= begin) {
+            begin = it->first;
+            it->second = std::max(end, it->second);
+            ranges.erase(++it);
         }
-        ranges.erase(ranges.begin(), it);
+    }
+
+    it = ranges.find(begin);
+    for(; it != ranges.end() && end >= it->first;) {
+        end = std::max(end, it->second);
+        ranges.erase(it++);
+    }
+    ranges[begin] = end;
+
+    if(begin == total_assembled) { // put contiguous string in stream
+        total_assembled = end;
+        ranges.erase(begin);
 
         string s;
         for(size_t i=begin; i<total_assembled; i++) {
@@ -51,13 +65,9 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
 
 size_t StreamReassembler::unassembled_bytes() const { 
     size_t sum = 0;
-    auto it = ranges.begin();
-    size_t end = it->first;
-    for(; it != ranges.end(); ++it) {
-        if(it->second > end) {
-            sum += it->second - std::max(it->first, end);
-        }
-        end = std::max(it->second, end);
+    
+    for(auto it: ranges) {
+        sum += it.second - it.first;
     }
     return sum; 
 }
