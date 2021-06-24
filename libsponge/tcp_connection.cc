7b3148602f188770551483c1286a9bfa4216f3be
superestos <superestos@gmail.com>
2021-06-25 01:04:38 +0800

    simplify clean shutdown condition

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -118,8 +118,8 @@ bool TCPConnection::active() const {
     }
 
     //clean shutdown
-    if ( (_sender.stream_in().eof() && _sender.bytes_in_flight() == 0 && _sender.next_seqno_absolute() == _sender.stream_in().bytes_written() + 2 ) &&
-         (_receiver.stream_out().input_ended() && _receiver.unassembled_bytes() == 0) &&
+    if ( (_sender.stream_in().eof() && _sender.bytes_in_flight() == 0) &&
+         (_receiver.stream_out().eof()) &&
          (!_linger_after_streams_finish || time_since_last_segment_received() >= 10 * _cfg.rt_timeout) ) {
         return false;
     }
